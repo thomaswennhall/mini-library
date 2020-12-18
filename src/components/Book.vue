dc53a8d1688fe5f0e02b3e2ca74ec1be89b9af50
thomaswennhall <70705571+thomaswennhall@users.noreply.github.com>
2020-12-18 15:30:48 +0100

    Skapat ett projekt i terminalen
    Fixat så så att vi kommer åt data från books.json och lagt till lite basic grejer

---
+++ src/components/Book.vue
@@ -0,0 +1,18 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    computed: {
+        books(){
+        return this.$root.books
+    }},
+    data() { return {
+    }}
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Books from './assets/books.json'
 
 Vue.config.productionTip = false
 
 new Vue({
+  data(){ return {
+    books: Books.books
+  }},
+
   router,
   render: h => h(App)
 }).$mount('#app')
